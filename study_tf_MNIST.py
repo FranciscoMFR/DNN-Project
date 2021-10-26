66a6ba50758f779ee66ed48ff3a57b47c3d34dfc
FranciscoMFR <franciscor@ua.pt>
2021-10-26 19:09:29 +0100

    compiling the model and fitting the data

--- study_tf_MNIST.py
+++ study_tf_MNIST.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
+from tensorflow.python.keras.backend import batch_dot
 
 
 # tensorflow.keras.Sequential
@@ -16,6 +17,10 @@ model = tf.keras.Sequential(
         Conv2D(128, (3,3), activation='relu'),
         MaxPool2D(),
         BatchNormalization(),
+
+        GlobalAvgPool2D(),
+        Dense(64, activation='relu'),
+        Dense(10, activation='softmax')
     ]
 )
 
@@ -48,4 +53,22 @@ if __name__=='__main__':
     print("x_test.shape = ", x_test.shape)
     print("y_test.shape = ", y_test.shape)
 
-    display_some_examples(x_train, y_train)
+    if False:
+        display_some_examples(x_train, y_train)
+
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy') #categorical_crossentropy -> use one hot coding
+
+    #model training
+    model.fit(x_train, y_train, batch_size=64, epochs=3, validation_split=0.2)
+    
+    #evaçuation on test set
+    model.evaluate(x_test, y_test, batch_size=64)
