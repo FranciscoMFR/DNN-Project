4061de3c97f7f850e04b0d71fa0f9a04b339cf25
FranciscoMFR <franciscor@ua.pt>
2021-10-29 15:46:30 +0100

    Pimp code & preparing test set

--- my_utils.py
+++ my_utils.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import glob
+from sklearn.model_selection import train_test_split
+import shutil
+import csv
 
 
 
@@ -19,3 +24,63 @@ def display_some_examples(examples, labels):
         plt.imshow(img, cmap='gray')
 
     plt.show()
+
+
+def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.1):
+
+    folders = os.listdir(path_to_data)
+
+    for folder in folders:
+
+        full_path = os.path.join(path_to_data, folder)
+        images_paths = glob.glob(os.path.join(full_path, '*.png'))
+        print(folder)
+
+        x_train, x_val = train_test_split(images_paths, test_size=split_size)
+
+        for x in x_train:
+
+            path_to_folder = os.path.join(path_to_save_train, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
+
+        for x in x_val:
+
+            path_to_folder = os.path.join(path_to_save_val, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
+
+
+
+def order_test_set(path_to_images, path_to_csv):
+    
+    try:
+        with open(path_to_csv, 'r') as csvfile:
+
+            reader = csv.reader(csvfile, delimiter=',')
+
+            for i, row in enumerate(reader):
+                
+                if i == 0:
+                    continue
+
+                img_name = row[-1].replace('Test/', '')
+                label = row[-2]
+                print(label)
+
+                path_to_folder = os.path.join(path_to_images, label)
+
+                if not os.path.isdir(path_to_folder):
+                    os.makedirs(path_to_folder)
+
+                img_full_path = os.path.join(path_to_images, img_name)
+                shutil.move(img_full_path, path_to_folder)
+
+    except:
+        print('[INFO] : Error reading csv file')
