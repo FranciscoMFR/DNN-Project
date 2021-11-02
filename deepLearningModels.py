94a853275a33280d4504f011d84d98be1847770a
FranciscoMFR <franciscor@ua.pt>
2021-11-02 17:20:05 +0000

    Same

--- deepLearningModels.py
+++ deepLearningModels.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
+from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D, Flatten
+from tensorflow.keras import Model
 
 
 
@@ -58,3 +59,34 @@ class MyCustomModel(tf.keras.Model):
         x = self.dense2(x)
 
         return x
+
+
+def street_signs_model(nbr_classes):
+
+    my_input = Input(shape=(60,60,3))
+    
+    x = Conv2D(32, (3, 3), activation='relu')(my_input)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+    
+    x = Conv2D(64, (3, 3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+
+    x = Conv2D(128, (3, 3), activation='relu')(x)
+    x = MaxPool2D()(x)
+    x = BatchNormalization()(x)
+
+    #x = Flatten()(x)
+    x = GlobalAvgPool2D()(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dense(nbr_classes, activation='softmax')(x)
+
+    return Model(inputs=my_input, outputs=x)
+
+
+
+if __name__=='__main__':
+
+    model = street_signs_model(10)
+    model.summary()
