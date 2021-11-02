295d50a4d14dc786f52b7d3762b2a51ea1e7c58e
FranciscoMFR <franciscor@ua.pt>
2021-11-02 18:03:40 +0000

    creating data generators

--- my_utils.py
+++ my_utils.py
@@ -5,6 +5,7 @@ import glob
 from sklearn.model_selection import train_test_split
 import shutil
 import csv
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 
@@ -57,7 +58,6 @@ def split_data(path_to_data, path_to_save_train, path_to_save_val, split_size=0.
             shutil.copy(x, path_to_folder)
 
 
-
 def order_test_set(path_to_images, path_to_csv):
     
     try:
@@ -84,3 +84,39 @@ def order_test_set(path_to_images, path_to_csv):
 
     except:
         print('[INFO] : Error reading csv file')
+
+
+def create_generators(batch_size, train_data_path, val_data_path, test_data_path):
+
+    preprocessor = ImageDataGenerator(
+        rescale = 1 /255.
+    )
+
+    train_generator = preprocessor.flow_from_directory(
+        train_data_path,
+        class_mode="categorical",
+        target_size=(60,60),
+        color_mode="rgb",
+        shuffle=True,
+        batch_size=batch_size
+    )
+
+    val_generator = preprocessor.flow_from_directory(
+        val_data_path,
+        class_mode="categorical",
+        target_size=(60,60),
+        color_mode="rgb",
+        shuffle=False,
+        batch_size=batch_size
+    )
+
+    test_generator = preprocessor.flow_from_directory(
+        test_data_path,
+        class_mode="categorical",
+        target_size=(60,60),
+        color_mode="rgb",
+        shuffle=False,
+        batch_size=batch_size
+    )
+
+    return train_generator, val_generator, test_generator
