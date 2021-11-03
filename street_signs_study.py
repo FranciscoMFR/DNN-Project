437156fcc934e95fb708d55c947be741619cab35
FranciscoMFR <franciscor@ua.pt>
2021-11-03 17:27:24 +0000

    Adding Callbacks

--- street_signs_study.py
+++ street_signs_study.py
@@ -2,6 +2,8 @@ import os
 import glob
 from sklearn.model_selection import train_test_split
 import shutil
+from tensorflow.keras import callbacks
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
 from my_utils import create_generators, split_data, order_test_set
 
@@ -15,16 +17,31 @@ if __name__ == '__main__':
     path_to_val_data = "C:\\Users\\User\\OneDrive - Universidade de Aveiro\\Desktop\\UA\\Projeto\\DNN-Project\\archive\\training_data\\val"
     path_to_test_data = "C:\\Users\\User\\OneDrive - Universidade de Aveiro\Desktop\\UA\\Projeto\\DNN-Project\\archive\\Test"
     batch_size=64
+    epochs = 100
 
-    train_generator, val_generator, test_generator = create_generators(path_to_train_data, path_to_val_data, path_to_test_data)
+    train_generator, val_generator, test_generator = create_generators(batch_size, path_to_train_data, path_to_val_data, path_to_test_data)
     nbr_classes = train_generator.num_classes
+
+    path_to_save_model = "./Models"
+    ckpt_saver = ModelCheckpoint(
+        path_to_save_model,
+        monitor="val_accuracy",
+        mode='max',
+        save_best_only=True,
+        save_freq='epoch',
+        verbose=1
+    )
+
+    early_stop = EarlyStopping(monitor='val_accuracy', patience=10)
 
     model = street_signs_model(nbr_classes)
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     model.fit(train_generator,         
-                epochs=3,
+                epochs=epochs,
                 batch_size=batch_size,
-                validation_data=val_generator
+                validation_data=val_generator,
+                callbacks=[ckpt_saver, early_stop]
             ) 
+
